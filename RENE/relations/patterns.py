ebf8faf12fffc2b219609a1ced6566c315589572
pidoux7 <faure.guillaume75@gmail.com>
2024-05-06 23:22:35 +0200

    p

---
+++ RENE/relations/patterns.py
@@ -0,0 +1,17 @@
+scheme = [
+    {
+        "subject": [{"label": "Chemical_and_drugs", "attr": {"Tech": [None]}}],
+        "object": [
+            {
+                "label": "Temporal",
+                "attr": {"AttTemp": ["Duration", "Date"]},
+            },
+            {
+                "label": "Chemical_and_drugs",
+                "attr": {"Tech": ["dosage", "route", "strength", "form"]},
+            },
+        ],
+        "type": "Depend",
+        "inv_type": "inv_Depend",
+    },
+]
